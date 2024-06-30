db12c04f84bf748438a08c7b15db716be0c5790c
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-30 23:17:00 +0300

    microservices security

--- src/main/java/com/anke/SpringBoot_Microservices_01_Product/security/SecurityConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.anke.SpringBoot_Microservices_01_Product.security;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class SecurityConfiguration {
-
-
-}
